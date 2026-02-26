155d0d051f1417ed1b0efe712ea2674e43224ef5
pbenav <pbenav@gmail.com>
2026-02-26 02:28:51 +0100

    fix: añadir botón 'Nueva compra' explícito en ListExpedicionesCompra

--- app/Filament/Resources/ExpedicionCompraResource/Pages/ListExpedicionesCompra.php
+++ app/Filament/Resources/ExpedicionCompraResource/Pages/ListExpedicionesCompra.php
@@ -4,11 +4,20 @@ namespace App\Filament\Resources\ExpedicionCompraResource\Pages;
 
 use App\Filament\Resources\ExpedicionCompraResource;
 use App\Filament\Resources\ExpedicionCompraResource\Widgets\ExpedicionStatsWidget;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListExpedicionesCompra extends ListRecords
 {
     protected static string $resource = ExpedicionCompraResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->label('Nueva compra'),
+        ];
+    }
 
     protected function getHeaderWidgets(): array
     {
