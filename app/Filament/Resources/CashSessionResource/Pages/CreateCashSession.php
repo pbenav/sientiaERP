a64932a0d9a7ca21ee1146d753d2aedaeb701189
Pablo Benavides <pbenav@gmail.com>
2026-02-26 18:04:22 +0100

    fix(pos): ensure user_id and mandatory fields are set when creating cash session from admin panel

--- app/Filament/Resources/CashSessionResource/Pages/CreateCashSession.php
+++ app/Filament/Resources/CashSessionResource/Pages/CreateCashSession.php
@@ -9,4 +9,13 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCashSession extends CreateRecord
 {
     protected static string $resource = CashSessionResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+        $data['fecha_inicio'] = now();
+        $data['estado'] = 'open';
+
+        return $data;
+    }
 }
